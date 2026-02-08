5cc7c5399812b5801c7fd1703aa479345efdb65a
akamugin <stella.lee@vanderbilt.edu>
2026-02-07 23:39:41 -1000

    Initial Commit

---
+++ DailyOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyOS
+//
+//  Created by Stella Lee on 2/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyOS/DailyOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyOSApp.swift
+//  DailyOS
+//
+//  Created by Stella Lee on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
